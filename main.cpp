5fd8e12ae4b331b969a5a7444a95718cbe6542e7
Savin Kirill <savin.kirill.mo@yandex.ru>
2020-01-30 16:39:58 +0300

    dynamic buffer resizing, better structure

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "server.h"
+#include "threadpool.h"
+
+void parseArguments(int argc, char** argv, char*& h, char*& p, char*& d) {
+    int rez = 0;
+    while ((rez = getopt(argc, argv, "p:h:d:")) != -1) {
+        switch (rez) {
+            case 'h':
+                h = optarg;
+                break;
+            case 'p':
+                p = optarg;
+                break;
+            case 'd':
+                d = optarg;
+                break;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    char *h = nullptr, *p = nullptr, *d = nullptr;
+    parseArguments(argc, argv, h, p, d);
+    if (h == nullptr || p == nullptr || d == nullptr) {
+        std::cerr << "usage: server -h <ip> -p <port> -d <directory>\n";
+        exit(1);
+    }
+
+    chroot(d);
+    chroot(d);
+
+    NetworkManager manager(h, p);
+
+    ThreadPool pool(2);
+
+    while (true) {
+        auto client = manager.waitClient();
+
+        auto serveFunc = [cli = std::move(client)]() mutable {
+            int ret = cli.getRequest();
+            std::cout << ret << std::endl;
+            if (ret >= 0) cli.giveResponse();
+        };
+
+        pool.addTask(std::move(serveFunc));
+    }
+
+    return 0;
+}
